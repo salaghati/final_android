7073558dbb4169408ce6261328fc00df22fbe2a6
salaghati <Salaghati2003>
2023-05-22 20:14:46 +0700

    update

--- WeatherAppInAndroidStudio/app/src/main/java/com/example/weatherapptutorial/cityFinder.java
+++ WeatherAppInAndroidStudio/app/src/main/java/com/example/weatherapptutorial/cityFinder.java
@@ -1,39 +1,52 @@
-package com.example.weatherapptutorial;
+package com.example.weatherapptutorial;    // package name
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;    // import appcompat activity
 
-import android.content.Intent;
+import android.content.Intent;    // import intent
 import android.os.Bundle;
-import android.view.KeyEvent;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.view.KeyEvent;    // import key event
+import android.view.View;    // import view
+import android.widget.EditText;    // import edit text
+import android.widget.ImageButton;    // import image button
+import android.widget.ImageView;    // import image view
+import android.widget.TextView;    // import text view
 
-public class cityFinder extends AppCompatActivity {
+public class cityFinder extends AppCompatActivity {    // class city finder
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_city_finder);
-        final EditText editText=findViewById(R.id.searchCity);
-        ImageView backButton=findViewById(R.id.backButton);
+    protected void onCreate(Bundle savedInstanceState) {    // oncreate method
+    /*
+    Function to create the activity.
 
-        backButton.setOnClickListener(new View.OnClickListener() {
+    Parameters
+    ----------
+    savedInstanceState: Bundle
+        Bundle to save the state of the activity.
+
+    Returns
+    -------
+    None
+
+    */
+        super.onCreate(savedInstanceState);    // call super method
+        setContentView(R.layout.activity_city_finder);    // set content view
+        final EditText editText=findViewById(R.id.searchCity);    // initializing edittext
+        ImageView backButton=findViewById(R.id.backButton);    // initializing image view
+
+        backButton.setOnClickListener(new View.OnClickListener() {    // set on click listener
             @Override
-            public void onClick(View v) {
-                finish();
+            public void onClick(View v) {    // on click method
+                finish();    // finish current activity
             }
         });
 
-        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {    // set on editor action listener
             @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                String newCity= editText.getText().toString();
-                Intent intent=new Intent(cityFinder.this,MainActivity.class);
-                intent.putExtra("City",newCity);
-                startActivity(intent);
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {    // on editor action method
+                String newCity= editText.getText().toString();    // get text from edittext
+                Intent intent=new Intent(cityFinder.this,MainActivity.class);    // create intent
+                intent.putExtra("City",newCity);    // put extra in intent
+                startActivity(intent);    // start activity
 
 
 
--- WeatherAppInAndroidStudio/app/src/main/java/com/example/weatherapptutorial/weatherData.java
+++ WeatherAppInAndroidStudio/app/src/main/java/com/example/weatherapptutorial/weatherData.java
@@ -1,109 +1,109 @@
-package com.example.weatherapptutorial;
+package com.example.weatherapptutorial;    // Package declaration
 
-import org.json.JSONException;
-import org.json.JSONObject;
+import org.json.JSONException;    // Import json exception
+import org.json.JSONObject;    // Import json object
 
-public class weatherData {
+public class weatherData {    // Defining weatherData class
 
-    private String mTemperature,micon,mcity,mWeatherType;
-    private int mCondition;
+    private String mTemperature,micon,mcity,mWeatherType;    // Declaring string variables
+    private int mCondition;    // Declaring integer variable
 
-    public static weatherData fromJson(JSONObject jsonObject)
+    public static weatherData fromJson(JSONObject jsonObject)    // Declaring static method fromJson
     {
 
-        try
+        try    // Try block
         {
-            weatherData weatherD=new weatherData();
-            weatherD.mcity=jsonObject.getString("name");
-            weatherD.mCondition=jsonObject.getJSONArray("weather").getJSONObject(0).getInt("id");
-            weatherD.mWeatherType=jsonObject.getJSONArray("weather").getJSONObject(0).getString("main");
-            weatherD.micon=updateWeatherIcon(weatherD.mCondition);
-            double tempResult=jsonObject.getJSONObject("main").getDouble("temp")-273.15;
-            int roundedValue=(int)Math.rint(tempResult);
-            weatherD.mTemperature=Integer.toString(roundedValue);
-            return weatherD;
+            weatherData weatherD=new weatherData();    // Defining object of weatherData class
+            weatherD.mcity=jsonObject.getString("name");    // Get String value of name from json object
+            weatherD.mCondition=jsonObject.getJSONArray("weather").getJSONObject(0).getInt("id");    // Get integer value of id from json object
+            weatherD.mWeatherType=jsonObject.getJSONArray("weather").getJSONObject(0).getString("main");    // Get String value of main from json object
+            weatherD.micon=updateWeatherIcon(weatherD.mCondition);    // Calling method updateWeatherIcon
+            double tempResult=jsonObject.getJSONObject("main").getDouble("temp")-273.15;    // Get double value of temp from json object
+            int roundedValue=(int)Math.rint(tempResult);    // Round off value of temp
+            weatherD.mTemperature=Integer.toString(roundedValue);    // Convert integer variable to string
+            return weatherD;    // return weatherD object
         }
 
 
-         catch (JSONException e) {
-            e.printStackTrace();
-            return null;
+        catch (JSONException e) {    // catch block
+            e.printStackTrace();    // Print stack trace
+            return null;    // return null
         }
 
 
     }
 
 
-    private static String updateWeatherIcon(int condition)
+    private static String updateWeatherIcon(int condition)    // Declaring static method updateWeatherIcon
     {
-        if(condition>=0 && condition<=300)
+        if(condition>=0 && condition<=300)    // Checking if condition is in range 0 to 300
         {
-            return "thunderstrom1";
+            return "thunderstrom1";    // return string value thunderstrom1
         }
-        else if(condition>=300 && condition<=500)
+        else if(condition>=300 && condition<=500)    // Checking if condition is in range 300 to 500
         {
-            return "lightrain";
+            return "lightrain";    // return string value lightrain
         }
-        else if(condition>=500 && condition<=600)
+        else if(condition>=500 && condition<=600)    // Checking if condition is in range 500 to 600
         {
-            return "shower";
+            return "shower";    // return string value shower
         }
-       else  if(condition>=600 && condition<=700)
+        else  if(condition>=600 && condition<=700)    // Checking if condition is in range 600 to 700
         {
-            return "snow2";
+            return "snow2";    // return string value snow2
         }
-        else if(condition>=701 && condition<=771)
+        else if(condition>=701 && condition<=771)    // Checking if condition is in range 701 to 771
         {
-            return "fog";
-        }
-
-        else if(condition>=772 && condition<=800)
-        {
-            return "overcast";
-        }
-       else if(condition==800)
-        {
-            return "sunny";
-        }
-        else if(condition>=801 && condition<=804)
-        {
-            return "cloudy";
-        }
-       else  if(condition>=900 && condition<=902)
-        {
-            return "thunderstrom1";
-        }
-        if(condition==903)
-        {
-            return "snow1";
-        }
-        if(condition==904)
-        {
-            return "sunny";
-        }
-        if(condition>=905 && condition<=1000)
-        {
-            return "thunderstrom2";
+            return "fog";    // return string value fog
         }
 
-        return "dunno";
+        else if(condition>=772 && condition<=800)    // Checking if condition is in range 772 to 800
+        {
+            return "overcast";    // return string value overcast
+        }
+        else if(condition==800)    // Checking if condition is 800
+        {
+            return "sunny";    // return string value sunny
+        }
+        else if(condition>=801 && condition<=804)    // Checking if condition is in range 801 to 804
+        {
+            return "cloudy";    // return string value cloudy
+        }
+        else  if(condition>=900 && condition<=902)    // Checking if condition is in range 900 to 902
+        {
+            return "thunderstrom1";    // return string value thunderstrom1
+        }
+        if(condition==903)    // Checking if condition is 903
+        {
+            return "snow1";    // return string value snow1
+        }
+        if(condition==904)    // Checking if condition is 904
+        {
+            return "sunny";    // return string value sunny
+        }
+        if(condition>=905 && condition<=1000)    // Checking if condition is in range 905 to 1000
+        {
+            return "thunderstrom2";    // return string value thunderstrom2
+        }
+
+        return "dunno";    // return string value dunno
 
 
     }
 
-    public String getmTemperature() {
-        return mTemperature+"°C";
+    public String getmTemperature() {    // Declaring getmTemperature method
+        return mTemperature+"°C";    // Return string value of mTemperature
     }
 
-    public String getMicon() {
-        return micon;
+    public String getMicon() {    // Declaring getMicon method
+        return micon;    // Return string value of micon
     }
 
-    public String getMcity() {
-        return mcity;
+    public String getMcity() {    // Declaring getMcity method
+        return mcity;    // Return string value of mcity
     }
 
-    public String getmWeatherType() {
-        return mWeatherType;
+    public String getmWeatherType() {    // Declaring getmWeatherType method
+        return mWeatherType;    // Return string value of mWeatherType
     }
 }
